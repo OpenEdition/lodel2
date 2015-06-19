327ae6a1e91a5250e37572ca2a5d5f26aa30c3c2
ArnAud <arnaud.cordier@openedition.org>
2015-06-19 17:18:24 +0200

    EmClassType : rewrite of the dict representation, better documentation

--- EditorialModel/classtypes.py
+++ EditorialModel/classtypes.py
@@ -1,55 +1,72 @@
 # -*- coding: utf-8 -*-
 
-"""
-    representation of the classTypes (sic…)
-"""
-
+## constant name for the nature of type hierarchy
 class EmNature(object):
     PARENT = 'parent'
     TRANSLATION = 'translation'
     IDENTITY = 'identity'
 
+##  Representation of the classTypes
+#
+#    Defines 3 generic classtype : entity, entry and person
+#        - entity : to define editorial content
+#        - entry  : to define keywords
+#        - person : to define people (in the real world, this classtype will only have one class and one type)
+#
+#    The hierarchy dict fixes the possible hierarchical links the types of each classtype can have :
+#       - 'attach' : what type of superior a type can have
+#            - 'classtype' a type can have superiors of the same classtype
+#            - 'type'      a type can only have superiors of the same type
+#       - automatic : possible superiors 
+#            - False : possible superiors must be defined
+#            - True  : possible superiors can not be defined, they will be enforced by the ME automatically
+#       - maxdepth : maximum depth
+#       - maxchildren : maximum children
+#
 class EmClassType(object):
 
     entity = {
         'name' : 'entity',
-        'hierarchy' : [
-            {
-                'nature'   : EmNature.PARENT,
+        'hierarchy' : {
+            EmNature.PARENT : {
+                'attach'   : 'classtype',
+                'automatic' : False,
+                'maxdepth' : -1,
+                'maxchildren' : -1
+                },
+            EmNature.TRANSLATION : {
                 'attach'   : 'type',
-                'editable' : True,
-            },
-            {
-                'nature'   : EmNature.TRANSLATION,
-                'attach'   : 'type',
-                'editable' : True,
-            },
-        ],
+                'automatic' : False,
+                'maxdepth' : 1,
+                'maxchildren' : -1
+                },
+        },
     }
 
     entry = {
         'name' : 'entry',
-        'hierarchy' : [
-            {
-                'nature'   : EmNature.PARENT,
+        'hierarchy' : {
+            EmNature.PARENT : {
                 'attach'   : 'type',
-                'editable' : True,
-            },
-            {
-                'nature'   : EmNature.TRANSLATION,
+                'automatic' : False,
+                },
+            EmNature.TRANSLATION : {
                 'attach'   : 'type',
-                'editable' : True,
-            },
-        ],
+                'automatic' : False,
+                'maxdepth' : 1,
+                'maxchildren' : -1
+                },
+        },
     }
 
     person = {
         'name' : 'person',
-        'hierarchy' : [
-            {
-                'nature'   : EmNature.IDENTITY,
+        'hierarchy' : {
+            EmNature.IDENTITY : {
                 'attach'   : 'classtype',
-                'editable' : False,
+                'automatic' : True,
+                'maxdepth' : -1,
+                'maxchildren' : 1
             },
-        ],
+        },
     }
