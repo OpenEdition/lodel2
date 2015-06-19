8e7a7ffe345473da452abcd309e44c02143a87c2
Roland Haroutiounian <roland.haroutiounian@openedition.org>
2015-06-19 13:33:59 +0200

    [#12] Modification de la classe EmField
    Suppression du init et passage des classes plutôt que les instances pour les fieldtypes dans le _fields

--- EditorialModel/fields.py
+++ EditorialModel/fields.py
@@ -21,26 +21,14 @@ class EmField(EmComponent):
 
     table = 'em_field'
     _fields = [
-        ('fieldtype', EmField_char()),
-        ('fieldgroup_id', EmField_integer()),
-        ('rel_to_type_id', EmField_integer()),
-        ('rel_field_id', EmField_integer()),
-        ('optional', EmField_boolean()),
-        ('internal', EmField_boolean()),
-        ('icon', EmField_integer())
+        ('fieldtype', EmField_char),
+        ('fieldgroup_id', EmField_integer),
+        ('rel_to_type_id', EmField_integer),
+        ('rel_field_id', EmField_integer),
+        ('optional', EmField_boolean),
+        ('internal', EmField_boolean),
+        ('icon', EmField_integer)
     ]
-
-    ## __init__ (Function)
-    #
-    # Instanciates an EmField object with data fetched from the database
-    #
-    # @param id_or_name str\int: Identifier of the EmField (global_id or name)
-    # @throw TypeError
-    # @see EmComponent::__init__()
-    def __init__(self, id_or_name):
-        self.table = EmField.table
-        self._fields = self.__class__._fields
-        super(EmField, self).__init__(id_or_name)
 
     ## Create (Function)
     #
