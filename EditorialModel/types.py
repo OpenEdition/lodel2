3f250baea837eee5c10e4fb0f609b168a8524b9c
Roland Haroutiounian <roland.haroutiounian@openedition.org>
2015-06-25 17:30:25 +0200

    Ecriture de la méthode LinkedTypes dans EmType

--- EditorialModel/types.py
+++ EditorialModel/types.py
@@ -222,5 +222,23 @@ class EmType(EmComponent):
     # @return a list of EmType
     # @see EmFields
     def linked_types(self):
-        pass
+        return self._linked_types_Db()
 
+    ## @brief Return the list of all the types linked to this type, should they be superiors or subordinates
+    # @return A list of EmType objects
+    def _linked_types_Db(self):
+        conn = self.getDbE().connect()
+        htable = self.__class__._tableHierarchy()
+        req = htable.select(htable.c.superior_id, htable.c.subordinate_id)
+        req = req.where(sql.or_(htable.c.subordinate_id == self.uid, htable.c.superior_id == self.uid))
+
+        res = conn.execute(req)
+        rows = res.fetchall()
+        conn.close()
+
+        rows = dict(zip(rows.keys(), rows))
+        result = []
+        for row in rows:
+            result.append(EmType(row['subordinate_id'] if row['superior_id']==self.uid else row['superior_id']))
+
+        return result
