c94c23f6e2a5297919d451397fa5a492814ba90b
Yann <yann.weber@openedition.org>
2015-07-23 09:14:23 +0200

    Added create_component delete_component to Model class

--- EditorialModel/model.py
+++ EditorialModel/model.py
@@ -26,6 +26,12 @@ class Model(object):
         self.components = {'uids':{}, 'class':{}, 'type':{}, 'field':{}, 'fieldgroup':{}}
         self.load()
 
+    @staticmethod
+    ## Given a name return an EmComponent child class
+    # @param class_name str : The name to identify an EmComponent class
+    def emclass_from_name(class_name):
+        return getattr(Model, class_name)
+
     ## Loads the structure of the Editorial Model
     #
     # Gets all the objects contained in that structure and creates a dict indexed by their uids
@@ -33,7 +39,7 @@ class Model(object):
         data = self.backend.load()
         for uid, component in data.items():
             cls_name = 'component_' + component['component']
-            cls = getattr(Model, cls_name)
+            cls = self.emclass_from_name(cls_name)
             if cls:
                 component['uid'] = uid
                 # create a dict for the component and one indexed by uids, store instanciated component in it
@@ -43,6 +49,22 @@ class Model(object):
     ## Saves data using the current backend
     def save(self):
         return self.backend.save()
+
+    ## Create a component from a component type and datas
+    #
+    # @param component_type str : a component type ( component_class, component_fieldgroup, component_field or component_type )
+    # @param datas dict : the options needed by the component creation
+    def create_component(self, component_type, datas):
+        return self.emclass_from_name[component_type].create(datas)
+
+    ## Delete a component
+    # @param uid int : Component identifier
+    # @throw NoSuchCompoentException
+    # @todo unable uid check
+    def delete_component(self, uid):
+        #if uid not in self.components:
+        #    raise NoSuchComponentException()
+        return self.components[uid].delete()
 
     ## Changes the current backend
     #
