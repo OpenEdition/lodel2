eb69492363050ad68b42a98428d2cc39540c6aba
Yann <yann.weber@openedition.org>
2015-07-23 14:35:59 +0200

    Deleted comments and bugfix on EmFieldGroup.fields()

--- EditorialModel/fieldgroups.py
+++ EditorialModel/fieldgroups.py
@@ -27,24 +27,6 @@ class EmFieldGroup(EmComponent):
     def __init__(self, data, model):
         super(EmFieldGroup, self).__init__(data, model)
 
-    # @classmethod
-    ## Create a new EmFieldGroup
-    #
-    # Save it in database and return an instance*
-    # @param **em_component_args : @ref EditorialModel::components::create(), must contain fields "name" (str) and "class" (EmClass)
-    # @throw EmComponentExistError If an EmFieldGroup with this name allready exists
-    # @throw TypeError If an argument is of an unexepted type
-    # def create(cls, **em_component_args):
-    #     fieldgroup_name = em_component_args['name']
-    #     fieldgroup_class = em_component_args['class']
-    #
-    #     if not isinstance(fieldgroup_name, str):
-    #         raise TypeError("Excepting <class str> as name. But got %s" % str(type(fieldgroup_name)))
-    #     if not isinstance(fieldgroup_class, EmClass):
-    #         raise TypeError("Excepting <class EmClass> as em_class. But got %s" % str(type(fieldgroup_class)))
-    #
-    #     return super(EmFieldGroup, cls).create(**em_component_args)
-
     ## Deletes a fieldgroup
     #
     # @return True if the deletion is a success, False if not
@@ -60,18 +42,10 @@ class EmFieldGroup(EmComponent):
     # @return A list of EmField instance
     def fields(self):
         result = []
-        for _, field in self.model.components(EmField):
+        for field in self.model.components(EmField):
             if field.fieldgroup_id == self.uid:
                 result.append(field)
         return result
-        # meta = sqlutils.meta(self.db_engine)
-        # field_table = sql.Table(EditorialModel.fields.EmField.table, meta)
-        # req = field_table.select(field_table.c.uid).where(field_table.c.fieldgroup_id == self.uid)
-        # conn = self.db_engine.connect()
-        # res = conn.execute(req)
-        # rows = res.fetchall()
-        # conn.close()
-        # return [EditorialModel.fields.EmField(row['uid']) for row in rows]
 
 class NotEmptyError(Exception):
     pass
