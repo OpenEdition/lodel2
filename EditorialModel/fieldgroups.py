9c5fad1b761c754563686d24ae5eec1cb2d4795b
Roland Haroutiounian <roland.haroutiounian@openedition.org>
2015-07-23 14:19:05 +0200

    [EmFieldGroup] Deactivated the create method (soon to be deleted from the class members)

--- EditorialModel/fieldgroups.py
+++ EditorialModel/fieldgroups.py
@@ -27,23 +27,23 @@ class EmFieldGroup(EmComponent):
     def __init__(self, data, model):
         super(EmFieldGroup, self).__init__(data, model)
 
-    @classmethod
+    # @classmethod
     ## Create a new EmFieldGroup
     #
     # Save it in database and return an instance*
     # @param **em_component_args : @ref EditorialModel::components::create(), must contain fields "name" (str) and "class" (EmClass)
     # @throw EmComponentExistError If an EmFieldGroup with this name allready exists
     # @throw TypeError If an argument is of an unexepted type
-    def create(cls, **em_component_args):
-        fieldgroup_name = em_component_args['name']
-        fieldgroup_class = em_component_args['class']
-
-        if not isinstance(fieldgroup_name, str):
-            raise TypeError("Excepting <class str> as name. But got %s" % str(type(fieldgroup_name)))
-        if not isinstance(fieldgroup_class, EmClass):
-            raise TypeError("Excepting <class EmClass> as em_class. But got %s" % str(type(fieldgroup_class)))
-
-        return super(EmFieldGroup, cls).create(**em_component_args)
+    # def create(cls, **em_component_args):
+    #     fieldgroup_name = em_component_args['name']
+    #     fieldgroup_class = em_component_args['class']
+    #
+    #     if not isinstance(fieldgroup_name, str):
+    #         raise TypeError("Excepting <class str> as name. But got %s" % str(type(fieldgroup_name)))
+    #     if not isinstance(fieldgroup_class, EmClass):
+    #         raise TypeError("Excepting <class EmClass> as em_class. But got %s" % str(type(fieldgroup_class)))
+    #
+    #     return super(EmFieldGroup, cls).create(**em_component_args)
 
     ## Deletes a fieldgroup
     #
