d6af3aaf4658d5d1a1dd2f887a756588e19a1eb4
Yann <yann.weber@openedition.org>
2015-07-24 12:07:03 +0200

    Fixing EmFieldGroup (replacing self._fields access by direct attribute access)

--- EditorialModel/fieldgroups.py
+++ EditorialModel/fieldgroups.py
@@ -49,12 +49,12 @@ class EmFieldGroup(EmComponent):
             em_type = self.model.component(type_id)
             fields = []
             for field in self.model.components(EmField):
-                if field.fieldgroup_id != self.uid or (field.optional and field.uid not in em_type._fields['selected_fields']):
+                if field.fieldgroup_id != self.uid or (field.optional and field.uid not in em_type.selected_fields):
                     continue
                 # don't include relational field if parent should not be included
                 if field.rel_field_id:
                     parent = self.model.component(field.rel_field_id)
-                    if parent.optional and parent.uid not in em_type._fields['selected_fields']:
+                    if parent.optional and parent.uid not in em_type.selected_fields:
                         continue
                 fields.append(field)
 
