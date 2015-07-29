33d163db8f93bb166732eb0b738bda7f09b5710c
Roland Haroutiounian <roland.haroutiounian@openedition.org>
2015-07-29 10:44:52 +0200

    [Exceptions] pep8+pylint on exceptions.py

--- EditorialModel/exceptions.py
+++ EditorialModel/exceptions.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+
 ## @brief An exception class to tell that a component don't exist
 class EmComponentNotExistError(Exception):
     pass
+
 
 ## @brief Raised on uniq constraint error at creation
 # This exception class is dedicated to be raised when create() method is called
@@ -10,13 +12,16 @@ class EmComponentNotExistError(Exception):
 class EmComponentExistError(Exception):
     pass
 
+
 ## @brief An exception class to tell that no ranking exist yet for the group of the object
 class EmComponentRankingNotExistError(Exception):
     pass
 
+
 ## @brief An exception class to return a failure reason for EmComponent.check() method
 class EmComponentCheckError(Exception):
     pass
+
 
 class MigrationHandlerChangeError(Exception):
     pass
