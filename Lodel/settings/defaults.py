f1a9a7d52056d2034b9124944ae2fcdd3cddd2c4
Yann <yann.weber@openedition.org>
2015-09-18 15:08:18 +0200

    Added new loggers settings

--- Lodel/settings/defaults.py
+++ Lodel/settings/defaults.py
@@ -29,10 +29,19 @@ ALLOWED_HOSTS = []
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s',
+        },
+        'simple': {
+            'format': '%(levelname)s %(module)s %(funcName)s %(message)s',
+        },
+    },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+            'formatter': 'simple',
         },
     },
     'loggers': {
@@ -45,6 +54,11 @@ LOGGING = {
             'handlers': ['console'],
             'level': 'DEBUG',
             'propagate': True,
+        },
+        'EditorialModel' : {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'DEBUG',
         },
     },
 }
