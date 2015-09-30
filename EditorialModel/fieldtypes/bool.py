5266c9ccf22e64d10bcd58cee1856c5024de89dd
Roland Haroutiounian <roland.haroutiounian@openedition.org>
2015-09-30 14:09:12 +0200

    PEP8/PyLint on fieldtypes/bool.py

--- EditorialModel/fieldtypes/bool.py
+++ EditorialModel/fieldtypes/bool.py
@@ -2,14 +2,15 @@
 
 from EditorialModel.fields import EmField
 
+
 class EmFieldBool(EmField):
-    
+
     ftype = 'bool'
     help = 'A basic boolean field'
-    
+
     ## @brief A char field
     # @brief max_length int : The maximum length of this field
     def __init__(self, **kwargs):
         super(EmFieldBool, self).__init__(**kwargs)
 
-fclass=EmFieldBool
+fclass = EmFieldBool
