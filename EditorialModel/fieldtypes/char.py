352735200d23485ad08e3d4e05d8b6675dfeaf8f
Roland Haroutiounian <roland.haroutiounian@openedition.org>
2015-09-30 14:10:27 +0200

    PEP8/PyLint on fieldtypes/char.py

--- EditorialModel/fieldtypes/char.py
+++ EditorialModel/fieldtypes/char.py
@@ -2,11 +2,12 @@
 
 from EditorialModel.fields import EmField
 
+
 class EmFieldChar(EmField):
-    
+
     ftype = 'char'
     help = 'Basic string (varchar) field. Take max_length=64 as option'
-    
+
     ## @brief A char field
     # @brief max_length int : The maximum length of this field
     def __init__(self, max_length=64, **kwargs):
