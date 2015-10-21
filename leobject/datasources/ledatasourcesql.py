e98e024916e934594cc6643a000554267448e9e6
Roland Haroutiounian <roland.haroutiounian@openedition.org>
2015-10-21 11:10:28 +0200

    [#61] Started using mosql

--- leobject/datasources/ledatasourcesql.py
+++ leobject/datasources/ledatasourcesql.py
@@ -1,11 +1,15 @@
 #-*- coding: utf-8 -*-
 
 from ledatasource import LeDataSource
+from mosql.db import Database
+from Lodel.utils.mosql import *
 
 ## SQL DataSource for LeObject
 class LeDataSourceSQL(LeDataSource):
 
     def __init__(self, options=None):
+        super(LeDataSourceSQL, self).__init__()
+            
         self.options = options
 
     ## @brief search for a collection of objects
