a935446871afab009713dfc03bf1cd6a20b35d52
Roland Haroutiounian <roland.haroutiounian@openedition.org>
2015-10-21 10:59:24 +0200

    [#61] added the LeDataSourceSQL class

--- leobject/datasources/ledatasourcesql.py
+++ leobject/datasources/ledatasourcesql.py
@@ -1,19 +1,17 @@
 #-*- coding: utf-8 -*-
 
-## Generic DataSource for LeObject
+from ledatasource import LeDataSource
 
-class LeDataSource(object):
+## SQL DataSource for LeObject
+class LeDataSourceSQL(LeDataSource):
 
     def __init__(self, options=None):
         self.options = options
 
-
     ## @brief search for a collection of objects
-    # @param emclass LeClass : LeClass instance
-    # @param emtype LeType : LeType instance
-    # @param field_list list : list of fields to get from the datasource
+    # @param emclass
+    # @param emtype
     # @param filters list : list of tuples formatted as (FIELD, OPERATOR, VALUE)
     # @param relational_filters
     def get(self, emclass, emtype, field_list, filters, relational_filters):
-        return False
-
+        pass
