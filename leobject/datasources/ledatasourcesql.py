dcd65f5f4c17fecf8afd39afa1ea45756ad77171
Roland Haroutiounian <roland.haroutiounian@openedition.org>
2015-10-23 11:06:29 +0200

    [#61] Rewriting the method headers

--- leobject/datasources/ledatasourcesql.py
+++ leobject/datasources/ledatasourcesql.py
@@ -14,35 +14,50 @@ class LeDataSourceSQL(DummyDatasource):
         self.db = Database(self.module, self.conn_args, self.conn_kargs)
 
     ## @brief update an existing object's data
-    # @param lodel_id int : lodel_id of the object to update
-    # @param checked_data dict
-    # @param filters
-    # @param relational_filters
-    def update (self, lodel_id, checked_data, filters, relational_filters):
+    # @param leclass LeClass
+    # @param filters list : list of tuples formatted as (FIELD, OPERATOR, VALUE)
+    # @param data dict
+    # @param relational_filters list : list of tuples formatted as (('superior'|'subordinate', FIELD), OPERATOR, VALUE)
+    # @param letype LeType
+    # @return bool
+    def update (self, leclass, filters, data, relational_filters=None, letype=None):
         pass
 
     ## @brief create a new object in the datasource
-    # @param letype LeType
     # @param leclass LeClass
-    # @param data dict : dictionnary of field:value pairs to save
-    # @return lodel_id int : lodel_id of the created object
-    def insert(self, letype, leclass, **data):
+    # @param letype LeType
+    # @param datas dict : dictionnary of field:value pairs to save
+    # @return int : lodel_id of the created object
+    def insert(self, leclass, letype=None, **datas):
         pass
 
     ## @brief delete an existing object
-    # @param lodel_id int : lodel_id of the object to delete
-    # @param filters list : list of typles formatted as (FIELD, OPERATOR, VALUE)
-    # @param relational_filters list
+    # @param leclass LeClass
+    # @param filters list : list of tuples formatted as (FIELD, OPERATOR, VALUE)
+    # @param relational_filters list : list of tuples formatted as (('superior'|'subordinate', FIELD), OPERATOR, VALUE)
+    # @param letype LeType
     # @return bool : True on success
-    def delete(self, lodel_id, filters, relational_filters):
+    def delete(self, leclass, filters, relational_filters=None, letype=None):
         pass
 
     ## @brief search for a collection of objects
-    # @param emclass
-    # @param emtype
+    # @param emclass LeClass
+    # @param emtype LeType
+    # @field_list list
     # @param filters list : list of tuples formatted as (FIELD, OPERATOR, VALUE)
-    # @param relational_filters
+    # @param relational_filters list : list of tuples formatted as (('superior'|'subordinate', FIELD), OPERATOR, VALUE)
     def get(self, emclass, emtype, field_list, filters, relational_filters=None):
+
+        tablename = emclass.name
+        where_filters = self._prepare_filters(filters)
+        if relational_filters or len(relational_filters) > 0:
+            for relational_filter in relational_filters:
+                relational_position = relational_filter[0][0]
+                relational_field = relational_filter[0][1]
+                relational_operator = relational_filter[1]
+                relational_value = relational_filter[2]
+
+
 
         tablename =  emclass.name
         where_filters = self._prepare_filters(filters)
