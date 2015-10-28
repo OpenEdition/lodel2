5738bb67cbab2b99ac04a8250dfaa3d12a9ba7b2
Roland Haroutiounian <roland.haroutiounian@openedition.org>
2015-10-28 14:36:03 +0100

    PEP8/Pylint on LeClass

--- leobject/leclass.py
+++ leobject/leclass.py
@@ -1,11 +1,12 @@
 #-*- coding: utf-8 -*-
 
-import leobject
+#import leobject
+
 
 ## @brief Represent an EmClass data instance
-# @note Is not a derivated class of LeObject because the concrete class will be a derivated class from LeObect
+# @note Is not a derivated class of LeObject because the concrete class will be a derivated class from LeObject
 class LeClass(object):
-    
+
     ## @brief Stores fieldtypes by field name
     _fieldtypes = dict()
     ## @brief Stores relation with some LeType using rel2type fields. Key = rel2type fieldname value = LeType class
@@ -20,16 +21,15 @@ class LeClass(object):
     # @param **kwargs
     def __init__(self, **kwargs):
         raise NotImplementedError("Abstract class")
-    
+
     ## @brief Get the linked objects
     # @return an array of LeType derivated class instance
     def linked(self):
         pass
-    
+
     ## @brief Link this class with an LeObject
     # @param leo LeObject : The object to be linked with
     # @return True if success False allready done
     # @throw A Leo exception if the link is not allowed
     def link_to(self, leo):
         pass
-        
