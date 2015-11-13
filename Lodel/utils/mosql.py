4f1fc375eb515ef57ea9344ac28216bea95df595
ArnAud <arnaud.cordier@openedition.org>
2015-11-13 17:15:55 +0100

    mosql utils: no quote around column definitions

--- Lodel/utils/mosql.py
+++ Lodel/utils/mosql.py
@@ -7,10 +7,19 @@
 # define create, drop and alter Query to manipulate tables
 # define create_user, delete_user and grant to manipulate user and rights
 
-from mosql.util import Clause, Statement, Query, value, identifier, concat_by_comma, paren
+from mosql.util import Clause, Statement, Query, value, identifier, concat_by_comma, paren, raw
+
+def col_def(cols):
+    print("cols", cols)
+    ret = []
+    for col in cols:
+        print("col", col)
+        name, definition = col
+        ret.append(identifier(name) + ' ' + raw(definition))
+    return ret
 
 # chain definitions
-column_list = (identifier, concat_by_comma, paren)
+column_list = (col_def, concat_by_comma, paren)
 
 # Clauses
 create_clause = Clause('create table', alias='create', no_argument=True)
@@ -20,7 +29,7 @@ character_clause = Clause('CHARACTER SET', (value, ), alias='character')
 column_clause = Clause('column', column_list, alias='column', hidden=True)
 
 alter_clause = Clause('alter table', (identifier, ), alias='table')
-add_clause = Clause('add column', (identifier, ), alias='column')
+add_clause = Clause('add column', (col_def, ), alias='column')
 
 # Statements
 create_statement = Statement([create_clause, if_not_exists, table_clause, column_clause, character_clause])
