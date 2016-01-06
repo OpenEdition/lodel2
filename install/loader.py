f033b6c85b2a1e07230d00eb3e671edc164365cc
Yann <yann.weber@openedition.org>
2016-01-06 14:03:34 +0100

    Updated install/loader.py to handle settings

--- install/loader.py
+++ install/loader.py
@@ -1,18 +1,24 @@
-import settings
+import settings as instance_settings
 import importlib
 import sys
 import os
 
-sys.path.append(settings.lodel2_lib_path)
+sys.path.append(instance_settings.lodel2_lib_path)
+
+from Lodel.settings import Settings
+
+# Update the settings
+for name in [ name for name in dir(instance_settings) if not name.startswith('__') ]:
+    Settings.set(name, getattr(instance_settings, name))
 
 # Import dynamic code
-if os.path.isfile(settings.dynamic_code):
+if os.path.isfile(Settings.get('dynamic_code')):
     from dynleapi import *
 
 # Import wanted datasource objects
 for db_modname in ['leapidatasource', 'migrationhandler']:
     mod = importlib.import_module("DataSource.{pkg_name}.{mod_name}".format(
-            pkg_name=settings.ds_package,
+            pkg_name=Settings.get('ds_package'),
             mod_name=db_modname,
         )
     )
