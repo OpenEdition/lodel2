c5a5b10b4a90747349936edf42d2c8bbb99f538e
Yann <yann.weber@openedition.org>
2016-01-15 14:35:38 +0100

    Untested implementation of join fieldtype and format fieldtype

--- EditorialModel/fieldtypes/join.py
+++ EditorialModel/fieldtypes/join.py
@@ -1,11 +1,17 @@
 #-*- coding: utf-8 -*-
 
+import warnings
+
 from . import char
 
 class EmFieldType(char.EmFieldType):
     help = 'Automatic string field, defined as concatenation of other fields'
-
-    def __init__(self, field_list, max_length, **kwargs):
+    
+    ## @brief Instanciate a fieldtype designed to concatenate fields
+    # @param field_list list : fieldname list
+    # @param glue str : separator
+    # @param max_length int : Field max length
+    def __init__(self, field_list, max_length, glue = ' ', **kwargs):
         self._field_list = field_list
         super().__init__(internal='automatic', max_length = max_length)
 
@@ -13,4 +19,7 @@ class EmFieldType(char.EmFieldType):
         ret = ''
         for fname in self._field_list:
             ret += datas[fname]
+        if len(ret) > self.max_length:
+            warnings.warn("Join field overflow. Truncating value")
+            ret = [:self.max_length-1]
         return ret
