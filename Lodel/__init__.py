d774480aa665f62924bfdb13ecdc64b2e92829e0
Yann <yann.weber@openedition.org>
2016-01-27 16:58:55 +0100

    Updated the doxygen documentation
    Adding images support for doxygen documentation ( in doc/img ) + automated images generation with graphviz & make

--- Lodel/__init__.py
+++ Lodel/__init__.py
@@ -8,6 +8,7 @@
 #
 # @section mainpage_docs Documentation
 #
+# - Lodel2 architecture : @subpage lodel2_arch 
 # - Fieldtypes : @subpage lodel2_fieldtypes
 # @subsection mainpage_docs_leapi LeAPI
 #
@@ -27,4 +28,49 @@
 # - @ref leapi
 # - DataSource
 #  - DataSource.MySQL
+#
+
+## @page lodel2_arch Lodel2 architecture
+#
+#
+# @section lodel2_arch_basic Lodel2
+#
+# Lodel 2 is a CMS that allows to define content types ......
+#
+# @section lodel2_arch_edmod Lodel2 Editorial Model
+#
+# The editorial model is the part of lodel2 that allows to defines content types.
+#
+# To handle this the editorial model has 3  abstractions :
+# - Classes
+# - Types
+# - Fields
+#
+# @warning An editorial model does NOT contains values.
+#
+# The editorial model is used to generate the dynamic part of the Lodel2 API ( see @ref leapi )
+#
+# @subsection lodel2_arch_edmod_classes Editorial model classes ( EmClass )
+#
+# An EmClass is basically a named collection of EmFields ( see @ref lodel2_arch_edmod_fields ) associated with a classtype.
+#
+# Classtypes are "family" of EmClasses. They defines allowed hierarchical relations between EmClass.
 # 
+# @subsection lodel2_arch_edmod_types Editorial model types ( EmType )
+#
+# An EmType is a named EmClass specialization. In fact some EmField in an EmClass can be defined as optionnal and then be selected
+# or not to be included in an EmType.
+# 
+# @subsection lodel2_arch_edmod_fields Editorial model fields ( EmField )
+#
+# EmFields defines what kind of datas can be stored in EmTypes. Actually its the associationg with an EmFieldtype ( see @ref lodel2_fieldtypes )
+# that really defines what kind of datas can be stored.
+#
+# @subsection lodel2_arch_edmod_fig Editorial model figures
+#
+# @subsubsection lodel2_arch_edmod_fig_components Editorial model main components
+# @image html graphviz/em_components.png
+# @subsubsection lodel2_arch_edmod_fig_relations Editorial model relations between components
+# @image html graphviz/em_relations.png
+# @subsubsection lodel2_arch_edmod_fig_hierarchy Hierarchical relations between EmTypes given a classtype
+# @image html graphviz/em_types_hierarch.png
--- Makefile
+++ Makefile
@@ -1,21 +1,42 @@
 all: check doc pip
 
+# Running unit tests
 check:
 	python -m unittest -v
 
-doc: cleandoc
-	doxygen
-
+# Rule to update libs
 pip: cleanpycache
 	pip install --upgrade -r requirements.txt
 
+#
+# Documentation rules
+#
+graphviz_images_path = doc/img/graphviz
+
+doc: cleandoc docimages
+	doxygen
+
+# Generating graphviz images
+docimages:
+	cd $(graphviz_images_path); make
+
+#
+# Cleaning rules
+#
 .PHONY: check doc clean cleanpyc cleandoc cleanpycache
 
-cleandoc:
-	-rm -Rfv ./doc/
+clean: cleanpyc cleandoc cleanpycache
+
+# Documentation cleaning
+cleandoc: cleandocimages
+	-rm -Rfv ./doc/html ./doc/doxygen_sqlite3.db
+
+cleandocimages:
+	cd $(graphviz_images_path); make clean
+
+# Python cleaning
 cleanpyc:
 	-find ./ |grep -E "\.pyc$$" |xargs rm -fv 2>/dev/null
 cleanpycache: cleanpyc
 	-find ./ -type d |grep '__pycache__' | xargs rmdir -fv 2>/dev/null
 
-clean: cleanpyc cleandoc cleanpycache
