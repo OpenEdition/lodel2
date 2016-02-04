61547aff272309b741c65714630e4332595571be
Roland Haroutiounian <roland.haroutiounian@openedition.org>
2016-02-04 12:03:46 +0100

    Integration of a "controllers" module in the WebUI package

--- Interface/web/WebUI.py
+++ Interface/web/WebUI.py
@@ -1,15 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from Interface.Interface import Interface
+from .controllers import *
+
 
 ## @brief Web Interface Manager
-class WebUI(Interface):
+class WebUI(object):
 
     ## @brief Constructor
     #
     # @param request dict : request informations got from the server
     def __init__(self, request):
-        super(WebUI).__init__(request)
+        self.request = self.parse_request(request)
+
+    ## @brief parses the request informations received from the application
+    #
+    # @param request dict
+    # @return dict
+    def parse_request(self, request):
+        uri_args = request['REQUEST_URI'].split('/')
+        uri_args.pop(0)  # We delete the first element which is a ""
+        request['URL_ARGS'] = uri_args
+        return request
 
     def response(self):
-        pass
+        print(self.request['URL_ARGS'])
+        if self.request['URL_ARGS'][0] == 'admin':
+            return admin(self.request)
+        else:
+            return "Hello World"
