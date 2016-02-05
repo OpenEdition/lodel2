7fe3152699e683d2c5bd115a4cfaf114a349fc30
Roland Haroutiounian <roland.haroutiounian@openedition.org>
2016-02-05 17:14:29 +0100

    Plugins urls are taken into account in the main routing process

--- lodel_wsgi.py
+++ lodel_wsgi.py
@@ -1,3 +1,4 @@
+from importlib import import_module
 import re
 from urllib.parse import parse_qs
 
@@ -7,16 +8,32 @@ from Interface.web.urls import urls
 
 def application(env, start_response):
 
+    # TODO Placer cette conf dans les settings lodel
+    installed_apps = [
+        'testmod'
+    ]
+
+    # TODO Factoriser ce bloc de routing dans une classe dédiée
+    routes = []
+    for url in urls:
+        routes.append((url[0], url[1]))
+
+    for installed_app in installed_apps:
+        try:
+            mod = import_module('Plugins.%s.urls' % installed_app)
+            app_routes = getattr(mod, 'urls')
+            for app_route in app_routes:
+                routes.append(app_route)
+        except ImportError:
+            continue
+
     # Querystring
     env['GET'] = parse_qs(env.get('QUERY_STRING'))
-
     # POST Values
     env['POST'] = parse_qs(env.get('wsgi.input').read())
-
-
     # URL Args
     path = env.get('PATH_INFO', '').lstrip('/')
-    for regex, callback in urls:
+    for regex, callback in routes:
         match = re.search(regex, path)
         if match is not None:
             env['url_args'] = match.groups()
