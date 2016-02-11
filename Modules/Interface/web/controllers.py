62739b1c52281f9cfd4aea4accd21ba37f686a45
Roland Haroutiounian <roland.haroutiounian@openedition.org>
2016-02-11 11:53:11 +0100

    Restructured the run.py script and added a router module in the web Interface package

--- Modules/Interface/web/controllers.py
+++ Modules/Interface/web/controllers.py
@@ -4,16 +4,22 @@ from ...Template.Loader import TemplateLoader
 # This module contains the web UI controllers that will be called from the WebUI class
 
 
+response_codes = {
+    '200': '200 OK',
+    '404': '404 NOT FOUND'
+}
+
+
 def admin(request, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
+    start_response(response_codes['200'], [('Content-Type', 'text/html')])
     return [b"ADMIN"]
 
 
 def index(request, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
+    start_response(response_codes['200'], [('Content-Type', 'text/html')])
     return [b"DASHBOARD"]
 
 
 def not_found(request, start_response):
-    start_response('404 NOT FOUND', [('Content-Type', 'text/plain')])
+    start_response(response_codes['404'], [('Content-Type', 'text/plain')])
     return [b'Not Found']
