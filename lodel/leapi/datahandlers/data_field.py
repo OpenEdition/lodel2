51425bc319bd98b8250b8b3860b378bd79db6f14
Roland Haroutiounian <roland.har@gmail.com>
2016-03-18 16:50:17 +0100

    First implementation of data_field and field_data_handler modules

--- lodel/leapi/datahandlers/data_field.py
+++ lodel/leapi/datahandlers/data_field.py
@@ -4,5 +4,33 @@ from .field_data_handler import FieldDataHandler
 
 class DataField(FieldDataHandler):
 
-    def __init__(self):
-        pass
+    ## @brief Instanciates a new fieldtype
+    # @param nullable bool : is None allowed as value ?
+    # @param uniq bool : Indicates if a field should handle a uniq value
+    # @param primary bool : If true the field is a primary key
+    # @param internal str|False: if False, that field is not internal. Other values cans be "autosql" or "internal"
+    # @param **kwargs : Other arguments
+    # @throw NotImplementedError if called from bad class
+    def __init__(self, internal=False, nullable=True, uniq=False, primary=False, **kwargs):
+        if self.__class__ == DataField:
+            raise NotImplementedError("Abstract class")
+
+        super().__init__(internal, **kwargs)
+
+        self.nullable = nullable
+        self.uniq = uniq
+        self.primary = primary
+        if 'defaults' in args:
+            self.default, error = self.check_data_value(args['default'])
+            if error:
+                raise error
+            del(args['default'])
+
+    def check_data_value(self, value):
+        if value is None:
+            if not self.nullable:
+                return (None, TypeError("'None' value but field is not nullable"))
+
+            return (None, None)
+        return super().check_data_value(value)
+
--- lodel/leapi/datahandlers/field_data_handler.py
+++ lodel/leapi/datahandlers/field_data_handler.py
@@ -5,9 +5,30 @@ class FieldDataHandler(object):
 
     help_text = 'Generic Field Data Handler'
 
+    ## @brief List fields that will be exposed to the construct_data_method
+    _construct_datas_deps = []
+
     ## @brief constructor
-    def __init__(self):
-        pass
+    # @param internal False | str : define whether or not a field is internal
+    # @param immutable bool : indicates if the fieldtype has to be defined in child classes of LeObject or if it is designed globally and immutable
+    # @param **args
+    def __init__(self, internal=False, immutable=False, **args):
+        self.internal = internal  # Check this value ?
+        self.immutable = bool(immutable)
+
+        for argname, argval in args.items():
+            setattr(self, argname, argval)
+
+
+    ## Fieldtype name
+    @staticmethod
+    def name(cls):
+        return cls.__module__.split('.')[-1]
+
+    ## @brief checks if a fieldtype is internal
+    # @return bool
+    def is_internal(self):
+        return self.internal != False
 
     ## @brief calls the data_field defined _check_data_value() method
     # @return tuple (value, error|None)
@@ -31,3 +52,10 @@ class FieldDataHandler(object):
     @staticmethod
     def module_name(self, fieldtype_name):
         return 'leapi.datahandlers.data_fields.%s' % fieldtype_name
+
+    ## @brief __hash__ implementation for fieldtypes
+    def __hash__(self):
+        hash_dats = [self.__class__.__module__]
+        for kdic in sorted([k for k in self.__dict__.keys() if not k.startswith('_')]):
+            hash_dats.append((kdic, getattr(self, kdic)))
+        return hash(tuple(hash_dats))
