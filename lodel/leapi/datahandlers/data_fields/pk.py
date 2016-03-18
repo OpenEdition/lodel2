6ad872b7f7ad82a63c1ff69a377eb9a3bbdf1d09
Roland Haroutiounian <roland.har@gmail.com>
2016-03-18 15:47:35 +0100

    First implementation of regex string data field

--- lodel/leapi/datahandlers/data_fields/pk.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-from .integer import EmDataField as IntegerDataField
-
---
+++ lodel/leapi/datahandlers/data_fields/regexchar.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import re
+from .varchar import EmDataField as VarcharDataField
+
+
+class EmDataField(VarcharDataField):
+
+    help = 'String field validated with a regex. Takes two options : max_length and regex'
+
+    ## @brief A string field validated by a regex
+    # @param regex str : a regex string (passed as argument to re.compile())
+    # @param max_length int : the max length for this field (default : 10)
+    # @param **kwargs
+    def __init__(self, regex='', max_length=10, **kwargs):
+        self.regex = regex
+        self.compiled_re = re.compile(regex)  # trigger an error if invalid regex
+
+        super().__init__(max_length=max_length, **kwargs)
+
+    def _check_data_value(self, value):
+        error = None
+        if not self.compiled_re.match(value):
+            value = ''
+            error = TypeError('"%s" doesn\'t match the regex "%s"' % (value, self.regex))
+        return (value, error)
