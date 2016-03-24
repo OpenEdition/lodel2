a57c9e850051a5c4fbd3472775caf56be6fec84a
Roland Haroutiounian <roland.har@gmail.com>
2016-03-24 14:59:16 +0100

    Added a todo

--- lodel/leapi/datahandlers/relation.py
+++ lodel/leapi/datahandlers/relation.py
@@ -7,6 +7,7 @@ class Relation(FieldDataHandler):
     ## @brief Instanciates a Relation object
     # @param fieldname : the fieldname involved in the relation
     # @param reference : the reference corresponding to this relation
+    # @todo ajouter la récupération des objets correspondant pour le field et la référence
     def __init__(self, fieldname, reference, **kwargs):
         self.backref_fieldname = fieldname
         self.reference = reference
