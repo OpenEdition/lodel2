acf560e76167c82f80f21485cd70ccd3d256f1ec
Roland Haroutiounian <roland.har@gmail.com>
2016-03-25 11:11:55 +0100

    First implementation of Relation

--- lodel/leapi/datahandlers/relation.py
+++ lodel/leapi/datahandlers/relation.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
-from .field_data_handler import FieldDataHandler
+from lodel.leapi.datahandlers.field_data_handler import FieldDataHandler
+from lodel.editorial_model.components import EmField
 
 
 class Relation(FieldDataHandler):
 
     ## @brief Instanciates a Relation object
-    # @param fieldname : the fieldname involved in the relation
-    # @param reference : the reference corresponding to this relation
-    # @todo ajouter la récupération des objets correspondant pour le field et la référence
-    def __init__(self, fieldname, reference, **kwargs):
-        self.backref_fieldname = fieldname
-        self.reference = reference
+    # @param datahandler FieldDataHandler
+    # @param datahandler_args dict
+    # @param reference Reference
+    # @param kwargs
+    def __init__(self, datahandler, datahandler_args, reference, **kwargs):
+        # TODO uid ?
+        self.backref_field = EmField(data_handler=datahandler, **datahandler_args)
+        self.backref_ref = reference
         super().__init__(**kwargs)
+
+    def _check_data_value(self, value):
+        pass
+
+    def construct_data(self, emcomponent, fname, datas, cur_value):
+        pass
