036f16f8aa8eff6e5a5b6f4894de74c9dd561762
Roland Haroutiounian <roland.har@gmail.com>
2016-03-25 14:42:22 +0100

    Added the username and password management for the mongodb connection string

--- lodel/datasource/mongodb_datasource.py
+++ lodel/datasource/mongodb_datasource.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
 import pymongo
 from pymongo import MongoClient
+import urllib
 
-
+# TODO Positionner cette variable dans les settings
 DEFAULT_CONNECTION = {
     'host': 'localhost',
     'port': 27017,
+    'login': 'login',  # TODO modifier la valeur
+    'password': 'password',  # TODO modifier la valeur
     'dbname': 'lodel'
 }
 
 class MongoDbDataSource(object):
 
     def __init__(self, module=pymongo, connection_args=DEFAULT_CONNECTION):
-        self.connection = MongoClient(connection_args['host'], connection_args['port'])
+        connection_string = 'mongodb://%s:%s@%s:%s' % (connection_args['login'],
+                                                       urllib.quote_plus(connection_args['password']),
+                                                       connection_args['host'],
+                                                       connection_args['port'])
+        self.connection = MongoClient(connection_string)
         self.database = self.connection[connection_args['dbname']]
 
     def insert(self):
