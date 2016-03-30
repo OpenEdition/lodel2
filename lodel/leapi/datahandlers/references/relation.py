c7695e0166ec74f3b0e44d959e8d4d99be32cd4d
Roland Haroutiounian <roland.har@gmail.com>
2016-03-30 09:55:43 +0200

    Moved relation module in references package

--- lodel/leapi/datahandlers/references/relation.py
+++ lodel/leapi/datahandlers/references/relation.py
@@ -3,15 +3,15 @@ from lodel.leapi.datahandlers.field_data_handler import FieldDataHandler
 from lodel.editorial_model.components import EmField
 
 
-class Relation(FieldDataHandler):
+class DataHandler(FieldDataHandler):
 
     ## @brief Instanciates a Relation object
     # @param datahandler FieldDataHandler
     # @param datahandler_args dict
-    # @param reference Reference
+    # @param reference str
     # @param kwargs
     def __init__(self, datahandler, datahandler_args, reference, **kwargs):
-        # TODO uid ?
+
         self.backref_field = EmField(data_handler=datahandler, **datahandler_args)
         self.backref_ref = reference
         super().__init__(**kwargs)
