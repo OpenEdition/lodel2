be57cf17c09567ae2c04ada4b5686b9fc0d08c03
Roland Haroutiounian <roland.har@gmail.com>
2016-04-01 14:40:08 +0200

    First implementation of the query class

--- lodel/leapi/query.py
+++ lodel/leapi/query.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 
-from .leobject import LeObject
+from leobject import LeObject
 
 
 class LeQueryError(Exception):
@@ -13,6 +13,10 @@ class LeQuery(object):
     ## @brief The datasource object used for this Query class
     _datasource = None
 
+    ## @brief the operators map
+    # assigns the right operator for a string based key (ex: "lte" => "<=" )
+    _query_operators_map = {}
+
     ## @brief Constructor
     # @param target_class LeObject class or childs : The LeObject child class concerned by this query
     def __init__(self, target_class):
@@ -21,8 +25,9 @@ class LeQuery(object):
         self._target_class = target_class
 
     ## @brief Prepares the query by formatting it into a dictionary
+    # @param datas dict: query parameters
     # @return dict : The formatted query
-    def prepare_query(self):
+    def prepare_query(self, datas=None):
         return {}
 
     ## @brief Executes the query
@@ -42,6 +47,17 @@ class LeInsertQuery(LeQuery):
         if target_class.is_abstract():
             raise LeQueryError("Target EmClass cannot be abstract for an InsertQuery")
 
+    def prepare_query(self, datas=None):
+        if datas is None or len(datas.keys()) == 0:
+            raise LeQueryError("No query datas found")
+
+        query = {}
+        query['action'] = self.__class__.action
+        query['target'] = self._target_class
+        for key, value in datas.items():
+            query[key] = value
+
+        return query
 
 ## @brief Handles Le*Query with a query_filter argument
 # @see LeGetQuery, LeUpdateQuery, LeDeleteQuery
@@ -65,3 +81,16 @@ class LeUpdateQuery(LeFilteredQuery):
 class LeDeleteQuery(LeFilteredQuery):
     # Name of the corresponding action
     action = 'delete'
+
+if __name__ == "__main__":
+    class MyObject(LeObject):
+        def __init__(self):
+            super().__init__()
+        @staticmethod
+        def is_abstract():
+            return False
+        def __str__(self):
+	    return 'MyObject'
+
+    my_insert_query = LeInsertQuery(MyObject)
+    print(my_insert_query.prepare_query({'test':'ok'}))
