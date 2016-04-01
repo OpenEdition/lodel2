12dd7a0d4ce4b1e3e56a3b2855398f2556dad623
Roland Haroutiounian <roland.har@gmail.com>
2016-04-01 10:11:21 +0200

    PEP8 + added the action class property on the query classes

--- lodel/leapi/query.py
+++ lodel/leapi/query.py
@@ -1,9 +1,11 @@
 #-*- coding: utf-8 -*-
 
-from leobject import LeObject
+from .leobject import LeObject
+
 
 class LeQueryError(Exception):
     pass
+
 
 ## @brief Handle CRUD operations on datasource
 class LeQuery(object):
@@ -15,27 +17,39 @@ class LeQuery(object):
             raise TypeError("target_class have to be a child class of LeObject")
         self._target_class = target_class
 
+
 ## @brief Handles insert queries
 class LeInsertQuery(LeQuery):
+
+    # Name of the corresponding action
+    action = 'insert'
 
     def __init__(self, target_class):
         super().__init__(target_class)
         if target_class.is_abstract():
             raise LeQueryError("Target EmClass cannot be abstract for an InsertQuery")
 
+
 ## @brief Handles Le*Query with a query_filter argument
 # @see LeGetQuery, LeUpdateQuery, LeDeleteQuery
 class LeFilteredQuery(LeQuery):
-    pass
+    # Name of the corresponding action
+    action = 'update'
+
 
 ## @brief Handles Get queries
 class LeGetQuery(LeFilteredQuery):
-    pass   
+    # Name of the corresponding action
+    action = 'get'
+
 
 ## @brief Handles Update queries
 class LeUpdateQuery(LeFilteredQuery):
-    pass
+    # Name of the corresponding action
+    action = 'update'
 
-## @biref Handles Delete queries
-class LeInsertQuery(LeFilteredQuery):
-    pass
+
+## @brief Handles Delete queries
+class LeDeleteQuery(LeFilteredQuery):
+    # Name of the corresponding action
+    action = 'delete'
