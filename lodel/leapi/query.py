b36062534b3dac626a05ac6947ac3a99996e8989
Roland Haroutiounian <roland.har@gmail.com>
2016-04-07 10:03:19 +0200

    First coding of the LeUpdateQuery class

--- lodel/leapi/query.py
+++ lodel/leapi/query.py
@@ -13,9 +13,8 @@ class LeQuery(object):
     ## @brief The datasource object used for this Query class
     _datasource = None
 
-    ## @brief the operators map
-    # assigns the right operator for a string based key (ex: "lte" => "<=" )
-    _query_operators_map = {}
+    ## @brief the operators used in query definitions
+    _query_operators_map = ['=', '<=', '>=', '!=', '<', '>', ' in ', ' not in ', ' like ', ' not like ']
 
     ## @brief Constructor
     # @param target_class LeObject class or childs : The LeObject child class concerned by this query
@@ -32,7 +31,7 @@ class LeQuery(object):
 
     ## @brief Executes the query
     # @return dict : The results of the query
-    def execute_query(self):
+    def execute(self):
         return {}
 
 
@@ -76,6 +75,64 @@ class LeGetQuery(LeFilteredQuery):
 class LeUpdateQuery(LeFilteredQuery):
     # Name of the corresponding action
     action = 'update'
+
+    def __init__(self, datas=None):
+        self.datas = datas if datas is None else locals()
+        del(self.datas['self'])
+
+    ## @brief executes the query, with corresponding hooks
+    # @return dict
+    def execute(self):
+        # LodelHook.call_hook('leapi_update_pre', self, None)
+        ret = self.update()
+        return self.after_update(ret)
+
+    ## @brief calls before-update hook(s)
+    # @return dict
+    # @todo to be implemented
+    def before_update(self):
+        return self.datas
+
+    ## @brief calls hook(s) after the update process
+    # @param ret : returned results
+    # @return bool : True if success
+    # @todo to be implemented
+    def after_update(self, ret):
+        return ret
+
+    ## @brief performs the update in the datasource
+    # @TODO the call to _datasource.update() will be changed when the corresponding method is implemented
+    # @TODO in case of an error, we should add code to manage it
+    def update(self):
+        if 'uid' not in self.datas:
+            raise LeQueryError("No object uid given to the query. The update can't be performed")
+        updated_datas = self.prepare_query()
+        ret = self._datasource.update(self.datas['uid'], **updated_datas)
+        if ret == 1:
+            return True
+        return False
+
+    ## @brief checks and prepare datas
+    # @return dict
+    def prepare_query(self):
+        ret_datas = self.check_datas_value(self.datas)
+        if isinstance(ret_datas, Exception):
+            raise ret_datas
+        ret_datas = self.construct_datas(ret_datas)
+        self.check_datas_consistency(ret_datas)
+        return ret_datas
+
+    def check_datas_value(self, datas):
+        err_l = dict()  # Stores all errors occurring during the process
+        correct = []  # Valid fields name
+        mandatory = []  # mandatory fields name
+        pass
+
+    def construct_datas(self, datas):
+        pass
+
+    def check_datas_consistency(self, datas):
+        pass
 
 
 ## @brief Handles Delete queries
