640f6653269522fdcc764f3b2ccbd4fa090a3c56
Yann <yann.weber@openedition.org>
2016-04-12 10:13:34 +0200

    More tests for SettingsLoader class

--- tests/settings/test_settings_loader.py
+++ tests/settings/test_settings_loader.py
@@ -2,8 +2,13 @@
 
 import unittest
 
-import tests.loader_utils
+#import tests.loader_utils
 from lodel.settings.settings_loader import SettingsLoader
+
+#A dummy validator that only returns the value
+def dummy_validator(value): return value
+#A dummy validator that always fails
+def dummy_validator_fails(value):  raise ValueError("Fake validaion error") 
 
 class SettingsLoaderTestCase(unittest.TestCase):
 
@@ -40,5 +45,32 @@ class SettingsLoaderTestCase(unittest.TestCase):
         g=settings.getremains()
         self.assertEqual(g,[])
         
+     
+    def test_getoption_simple(self):
+        """ Testing behavior of getoption """
+        loader = SettingsLoader('tests/settings/settings_examples/simple.conf.d')
+        value = loader.getoption('lodel2.foo.bar', 'foo', dummy_validator)
+        self.assertEqual(value, "42")
+        value = loader.getoption('lodel2.foo.bar', 'foobar', dummy_validator)
+        self.assertEqual(value, "hello world")
+
+    def test_variable_sections(self):
+        """ Testing variable section recognition """
+        loader = SettingsLoader('tests/settings/settings_examples/var_sections.conf.d')
+        sections = loader.getsection('lodel2.tests')
+        self.assertEqual(   set(sections),
+                            set((   'lodel2.tests.section1', 
+                                    'lodel2.tests.section2')))
+    
+    def test_variable_sections_default(self):
+        """ Testing variable section default value handling """
+        loader = SettingsLoader('tests/settings/settings_examples/var_sections.conf.d')
+        sections = loader.getsection('lodel2.notexisting', 'foobar')
+        self.assertEqual(set(sections), set(('lodel2.foobar',)))
         
-        
+    def test_variable_sections_fails(self):
+        """ Testing behavior when no default section given for a non existing variable section """
+        loader = SettingsLoader('tests/settings/settings_examples/var_sections.conf.d')
+        with self.assertRaises(NameError):
+            sections = loader.getsection('lodel2.notexisting')
+
