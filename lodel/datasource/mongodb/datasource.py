542fdf1ca79a859c3c5f536b6bdfeb2e8244ae97
Roland Haroutiounian <roland.har@gmail.com>
2016-04-13 14:20:48 +0200

    [MongoDbDataSource] Implementation of the connection creation and a first check on the given parameters
    - The check method checks, for now, if all the needed parameters are passed to the connection string
    - A test class has been written with some bad connection args examples

--- lodel/datasource/mongodb/datasource.py
+++ lodel/datasource/mongodb/datasource.py
@@ -14,42 +14,51 @@ DEFAULT_CONNECTION = {
     'dbname': 'lodel'
 }
 
+
+class MongoDbDataSourceError(Exception):
+    pass
+
+
 class MongoDbDataSource(object):
 
+    MANDATORY_CONNECTION_ARGS = ('host', 'post', 'login', 'password', 'dbname')
+
     def __init__(self, module=pymongo, connection_args=DEFAULT_CONNECTION):
-        connection_string = 'mongodb://%s:%s@%s:%s' % (connection_args['login'],
-                                                       urllib.quote_plus(connection_args['password']),
-                                                       connection_args['host'],
-                                                       connection_args['port'])
+        login, password, host, port, dbname = MongoDbDataSource._check_connection_args(connection_args)
+
+        # Creating of the connection
+        connection_string = 'mongodb://%s:%s@%s:%s' % (login, password, host, port)
         self.connection = MongoClient(connection_string)
-        self.database = self.connection[connection_args['dbname']]
+        # Getting the database
+        self.database = self.connection[dbname]
 
-    ##@brief Inserts a list of records in a given collection
-    #
-    # @param collection_name str : name of the MongoDB collection in which we will insert the records
-    # @param datas list : list of dictionaries corresponding to the records
-    # @throw BulkWriteError : is thrown when an error occurs during the execution of the ordered bulk operations
-    def insert(self, collection_name, datas):
-        collection = self.database[collection_name]
-        bulk = collection.initialize_ordered_bulk_op()
-        # TODO check if all the elements of the list are dictionaries
-        bulk.insert_many(datas)
-        try:
-            result = bulk.execute()
-        except BulkWriteError as bwe:
-            pass  # TODO Add the behavior in case of an exception => bwe.details['writeErrors'], is a list of error info dicts
+    ## @brief checks if the connection args are valid and complete
+    # @param connection_args dict
+    # @return bool
+    # @todo checks on the argument types can be added here
+    @classmethod
+    def _check_connection_args(cls, connection_args):
+        errors = []
+        for connection_arg in cls.MANDATORY_CONNECTION_ARGS:
+            if connection_arg not in connection_args:
+                errors.append("Datasource connection error : %s parameter is missing." % connection_arg)
+        if len(errors) > 0 :
+            raise MongoDbDataSourceError("\r\n-".join(errors))
 
-        return result['nInserted']
+        return (connection_args['login'], urllib.quote_plus(connection_args['password']), connection_args['host'],
+                connection_args['port'], connection_args['dbname'])
 
-    def select(self):
-
-        pass
-    
-    def update(self):
+    def select(self, target_cls, field_list, filters, rel_filters=None, order=None, group=None, limit=None, offset=0, instanciate=True):
         pass
 
     def delete(self, target_cls, uid):
-        uidname = target_cls.uidname
-        collection_name = target_cls.collection_name
-        result = self.database[collection_name].delete_many({uidname: uid})
-        return result.deteled_count
+        pass
+
+    def update(self, target_cls, uid, **datas):
+        pass
+
+    def insert(self, target_cls, **datas):
+        pass
+
+    def insert_multi(self, target_cls, datas_list):
+        pass
