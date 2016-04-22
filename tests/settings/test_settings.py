c439b6d9297f6c46865dffac2119f523dbb60ecb
prieto <helene.prieto@openedition.org>
2016-04-22 17:06:04 +0200

    test settings

--- tests/settings/test_settings.py
+++ tests/settings/test_settings.py
@@ -3,14 +3,27 @@
 import unittest
 from unittest import mock
 
-"""
 import tests.loader_utils
 from lodel.settings.settings import Settings
+from lodel.settings.settings import SettingsLoader
+
+def dummy_validator(value): return value
 
 class SettingsTestCase(unittest.TestCase):
     
-    @unittest.skip('todo : write it')
     def test_init(self):
-        settings = Settings('tests/settings/settings_tests.ini', 'tests/settings/settings_tests_conf.d')
-        pass
-"""
+        with self.assertRaises(RuntimeError):
+            Settings('tests/settings/settings_tests_conf.d')
+        
+    def test_set(self):
+        loader = SettingsLoader('globconf.d')
+        Settings.set('lodel2.editorialmodel.emfile','test ok', dummy_validator, loader)
+        Settings.set('lodel2.editorialmodel.editormode','test ok', dummy_validator, loader)
+        loader = SettingsLoader('globconf.d')
+        option = loader.getoption('lodel2.editorialmodel','emfile', dummy_validator)
+        self.assertEqual(option , 'test ok')
+        option = loader.getoption('lodel2.editorialmodel','editormode', dummy_validator)
+        self.assertEqual(option, 'test ok')
+        Settings.set('lodel2.editorialmodel.emfile','examples/em_test.pickle', dummy_validator, loader)
+        Settings.set('lodel2.editorialmodel.editormode','True', dummy_validator, loader)
+
