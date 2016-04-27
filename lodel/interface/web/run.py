872fc47724e6471390063232a412b52fc685e554
Roland Haroutiounian <roland.har@gmail.com>
2016-04-27 14:48:09 +0200

    Interface module's folder is created

---
+++ lodel/interface/web/run.py
@@ -0,0 +1,7 @@
+from werkzeug.wrappers import Response
+
+def application(environ, start_response):
+    request = Request(environ)
+    text = 'Hello %s!' % request.args.get('name', 'World')
+    response = Response(text, mimetype='text/plain')
+    return response(environ, start_response)
