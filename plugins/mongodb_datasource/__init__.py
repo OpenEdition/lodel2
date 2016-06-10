417ce0258a661b637f4d00b40521fb5fdfbbce16
Roland Haroutiounian <roland.har@gmail.com>
2016-06-10 11:54:59 +0200

    Added a default connection check in the _activate function of the mongodb datasource, to see if the plugin can connect

--- plugins/mongodb_datasource/__init__.py
+++ plugins/mongodb_datasource/__init__.py
@@ -1,3 +1,5 @@
+from .utils import connect, get_connection_args
+
 __loader__ = "main.py"
 __confspec__ = "confspec.py"
 __author__ = "Lodel2 dev team"
@@ -10,4 +12,13 @@ __fullname__ = "MongoDB plugin"
 #
 # @return bool|str : True if all the checks are OK, an error message if not
 def _activate():
+    default_connection_args = get_connection_args()
+    connection_check = connect(
+        default_connection_args['host'],
+        default_connection_args['port'],
+        default_connection_args['db_name'],
+        default_connection_args['username'],
+        default_connection_args['password'])
+    if not connection_check:
+        return False
     return True
