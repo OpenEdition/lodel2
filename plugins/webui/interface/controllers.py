1b9cb1a72d36ea1dddf81bd9eaebe74cdb695c67
Roland Haroutiounian <roland.har@gmail.com>
2016-06-15 16:52:22 +0200

    Changed the routing rules naming system

--- plugins/webui/interface/controllers.py
+++ plugins/webui/interface/controllers.py
@@ -5,21 +5,31 @@ from .template.loader import TemplateLoader
 
 # This module contains the web UI controllers that will be called from the web ui class
 
-def get_response(tpl, mimetype = 'text/html', status_code = 200):
+
+def get_response(tpl, mimetype='text/html', status_code=200):
     loader = TemplateLoader()
-    response = Response(    loader.render_to_response(tpl),
-                            mimetype = 'text/html')
+    response = Response(loader.render_to_response(tpl), mimetype=mimetype)
     response.status_code = status_code
     return response
+
 
 def admin(request):
     return get_response('admin/admin.html')
 
+
 def index(request):
     return get_response('index/index.html')
 
+
 def not_found(request):
-    return get_response('errors/404.html', status_code = 404)
+    return get_response('errors/404.html', status_code=404)
+
 
 def test(request):
     return get_response('test.html')
+
+
+def list_classes(request):
+    # TODO Add the method to get the classes list
+
+    return get_response('list_classes.html')
--- plugins/webui/interface/router.py
+++ plugins/webui/interface/router.py
@@ -3,12 +3,26 @@ import re
 
 from .controllers import *
 from .urls import urls
+from lodel.settings import Settings
+
+
+def format_url_rule(url_rule):
+    if url_rule == '^$':
+        return "^%s$" % Settings.sitename
+
+    formatted_rule = ''
+    if url_rule.startswith('^'):
+        formatted_rule += "^"
+
+    formatted_rule += "%s/%s" % (Settings.sitename, url_rule)
+    return formatted_rule
 
 
 def get_controller(request):
+
     url_rules = []
     for url in urls:
-        url_rules.append((url[0], url[1]))
+        url_rules.append((format_url_rule(url[0]), url[1]))
 
     # Returning the right controller to call
     for regex, callback in url_rules:
