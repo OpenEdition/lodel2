f386701392835c741b11d8b438be4b96d7def18a
Yann <yann.weber@openedition.org>
2016-06-17 14:56:25 +0200

    New headers for dummy MH

--- plugins/dummy_datasource/migration_handler.py
+++ plugins/dummy_datasource/migration_handler.py
@@ -2,14 +2,21 @@
 
 ##@brief Abtract class for migration handlers
 class DummyMigrationHandler(object):
+
+    ##@brief Create a new migration handler given DB connection options
     def __init__(self, *args, **kwargs):
         pass
     
+    ##@brief DB initialisation
+    #@param emclass_list list : list of EmClasses concerned by this MH
+    def init_db(self, emclass_list):
+        pass
+    
+    ##@todo redefine
     def register_change(self, model, uid, initial_state, new_state):
         pass
 
+    ##@todo redefine
     def register_model_state(self, em, state_hash):
         pass
-
-    def init_db(self):
-        pass
+    
