7aa9c214a8aa752e1a67a4fb0438a695cc355399
Roland Haroutiounian <roland.haroutiounian@openedition.org>
2016-06-30 14:17:03 +0200

    Added a get_component_html function to get an HTML code for a given gui component type

--- plugins/webui/interface/controllers/base.py
+++ plugins/webui/interface/controllers/base.py
@@ -11,6 +11,14 @@ def get_response(tpl='empty.html', tpl_vars={}, mimetype='text/html', status_cod
     response.status_code = status_code
     return response
 
+## @brief gets the html template corresponding to a given component type
+# @param type str : name of the component type
+# @param params dict : extra parameters to customize the template
+def get_component_html(type='text', params={}):
+    params['type'] = type
+    template_loader = TemplateLoader()
+    return template_loader.render_to_html(template_file='components/components.html', template_vars=params)
+
 def index(request):
     return get_response('index/index.html')
 
