6bcb9e1e7c094650de1bc8a23eeb5927783a6ce1
Roland Haroutiounian <roland.haroutiounian@openedition.org>
2016-07-07 15:11:28 +0200

    Deleted the Werkzeug sessions library calls for the filesystem session store

--- plugins/filesystem_session/main.py
+++ plugins/filesystem_session/main.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os
-from werkzeug.contrib.sessions import FilesystemSessionStore
+import pickle
+import uuid
 
 from lodel.plugin import LodelHook
 from lodel.settings import Settings
@@ -11,61 +12,76 @@ SESSION_FILES_BASE_DIR = Settings.sessions.directory
 SESSION_FILES_TEMPLATE = Settings.sessions.file_template
 SESSION_EXPIRATION_LIMIT = Settings.sessions.expiration
 
-session_store = FilesystemSessionStore(path=SESSION_FILES_BASE_DIR, filename_template=SESSION_FILES_TEMPLATE)
+
+## @brief generates a new session id
+# @return str
+def generate_new_sid():
+    new_sid = uuid.uuid1()
+    return new_sid
+
+## @brief gets the session file path corresponding to a session id
+def get_session_file_path(sid):
+    return os.path.join(SESSION_FILES_BASE_DIR, SESSION_FILES_TEMPLATE) % sid
+
+## @brief starts a new session and returns its sid
+# @param caller *
+# @param payload dict
+# @return str
+@LodelHook('session_start')
+def start_session(caller, payload):
+    sid = generate_new_sid()
+    session_file = get_session_file_path(sid)
+    session = dict()
+    for key, value in payload.items():
+        session[key] = value
+    pickle.dump(session, open(session_file, "wb"))
+    return sid
 
 
+## @brief stops a session
+# @param caller *
+# @param sid str : session id
+@LodelHook('session_stop')
+def stop_session(caller, sid):
+    session_file_path = get_session_file_path(sid)
+    if os.path.isfile(session_file_path):
+        os.unlink(session_file_path)
+
+
+## @brief checks if a session file has expired
+# @param sid str : session id
+# @return bool
+def is_session_file_expired(sid):
+    session_file = get_session_file_path(sid)
+    expiration_timestamp = os.stat(session_file).st_mtime + SESSION_EXPIRATION_LIMIT
+    timestamp_now = get_utc_timestamp()
+    return timestamp_now >= expiration_timestamp
+
+
+## @brief reads a session content
+# @param caller *
+# @param sid str : session id
+@LodelHook('session_read')
+def read_session(caller, sid):
+    session_file = get_session_file_path(sid)
+    if os.path.isfile(session_file) and not is_session_file_expired(sid):
+        session = pickle.load(open(session_file, "rb"))
+    else:
+        session = None
+
+    return session
+
+
+## @brief deletes all old session files (expired ones)
+# @param caller *
 @LodelHook('lodel_delete_old_session_files')
-def delete_old_session_files(caller, timestamp_now):
-    session_files_path = os.path.abspath(session_store.path)
+def delete_old_session_files(caller):
+    session_files_path = os.path.abspath(SESSION_FILES_BASE_DIR)
     session_files = [os.path.join(session_files_path, file_object) for file_object in os.listdir(session_files_path) if os.path.isfile(os.path.join(session_files_path, file_object))]
+    timestamp_now = get_utc_timestamp()
 
     for session_file in session_files:
         last_modified = os.stat(session_file).st_mtime
         expiration_timestamp = last_modified + SESSION_EXPIRATION_LIMIT
         if timestamp_now > expiration_timestamp:
             os.unlink(session_file)
-
-
-def is_session_file_expired(timestamp_now, sid):
-    session_file = session_store.get_session_filename(sid)
-    expiration_timestamp = os.stat(session_file).st_mtime + SESSION_EXPIRATION_LIMIT
-    return timestamp_now >= expiration_timestamp
-
-
-## @brief starts a new session
-# @param caller *
-# @param payload dict : dictionary containing the content of the session
-@LodelHook('lodel_start_session')
-def start_session(caller, payload):
-    session = session_store.new()
-    for key, value in payload.items():
-        session[key] = value
-    session_store.save(session)
-
-
-## @brief reads the content of the session
-# @param caller *
-# @param sid str
-# @return dict
-@LodelHook('lodel_read_session')
-def read_session(caller, sid):
-    timestamp = get_utc_timestamp()
-    session = None
-    if not is_session_file_expired(timestamp, sid):
-        session = session_store.get(sid)
-
-    return session
-
-
-@LodelHook('lodel_update_session')
-def update_session(caller, timestamp_now, sid):
-    if sid is None or sid not in session_store.list():
-        session = session_store.new()
-        session['last_accessed'] = timestamp_now
-    else:
-        session = session_store.get(sid)
-        if is_session_file_expired(timestamp_now, sid):
-            session_store.delete(session)
-            session = session_store.new()
-            session['user_context'] = None
-        session['last_accessed'] = timestamp_now
