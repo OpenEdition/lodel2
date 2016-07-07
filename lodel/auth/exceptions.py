82f12d95ec29ae9ec9daf7a660f677bc608175a3
Yann <yann.weber@openedition.org>
2016-07-07 16:22:38 +0200

    Authentication handlers implementation
    - implements a Client abstract singleton class designed to be implemented by UI to be able to register clients informations
    - implements a Auth singleton class. Kind of interface between Client singleton and session hanlder plugin

--- lodel/auth/exceptions.py
+++ lodel/auth/exceptions.py
@@ -1,12 +1,26 @@
 from lodel import logger
 
+##@brief Handles common errors on authentication
 class AuthenticationError(Exception):
     pass
 
+
+##@brief Handles authentication error with possible security issue
+#
+#@note Handle the creation of a security log message containing client info
+class AuthenticationSecurityError(AuthenticationError):
+    def __init__(self, client):
+        msg = "%s : authentication error" % client
+        logger.security(msg)
+        super().__init__(msg)
+
+
+##@brief Handles authentication failure
+#
+#@note Handle the creation of a security log message containing client info
 class AuthenticationFailure(Exception):
     
     def __init__(self, client):
         msg = "%s : authentication failure" % client
         logger.security(msg)
         super().__init__(msg)
-
