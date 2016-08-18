358c92c7a81b8c1102517efcbb9c3ead1c322f5d
Roland Haroutiounian <roland.har@gmail.com>
2016-08-18 10:31:49 +0200

    Added a test for the Regex DataHandler's can_override method

--- tests/datahandlers/test_regex.py
+++ tests/datahandlers/test_regex.py
@@ -1,19 +1,21 @@
 import unittest
 
-from lodel.leapi.datahandlers.datas import Regex, Varchar
+from lodel.leapi.datahandlers.datas import Regex, Varchar, Integer
 
 
 class RegexTestCase(unittest.TestCase):
 
     def test_check_correct_data_value(self):
         test_value = '126.205.255.12'
-        test_regex = Regex('^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', max_length=100)
+        test_regex = Regex('^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$',
+                           max_length=100)
         value, error = test_regex._check_data_value(test_value)
         self.assertIsNone(error)
         self.assertEqual(value, test_value)
 
     def test_check_bad_data_value(self):
-        test_regex = Regex('^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', max_length=15)
+        test_regex = Regex('^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$',
+                           max_length=15)
         for test_value in ['800.9.10.5', 'test_string_value', '999.999.999.999']:
             value, error = test_regex._check_data_value(test_value)
             self.assertEqual(value, '')
@@ -32,9 +34,16 @@ class RegexTestCase(unittest.TestCase):
                 self.assertEqual(value, test_value)
 
     def test_can_override(self):
-        test_regex = Regex('^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', max_length=15)
-        for test_varchar in [Varchar(max_length=64), Varchar(max_length=15), Varchar(max_length=9)]:
+        test_regex = Regex('^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$',
+                           max_length=15)
+        for test_varchar in [Varchar(), Varchar(max_length=15), Varchar(max_length=9)]:
             if test_regex.max_length == test_varchar.max_length:
                 self.assertTrue(test_regex.can_override(test_varchar))
             else:
                 self.assertFalse(test_regex.can_override(test_varchar))
+
+    def test_cant_override(self):
+        test_regex = Regex('^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$',
+                           max_length=15)
+        test_int = Integer()
+        self.assertFalse(test_regex.can_override(test_int))
