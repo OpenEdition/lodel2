46173c1dc9ab3782535322b052fc7a5420915df4
Roland Haroutiounian <roland.har@gmail.com>
2016-08-18 16:39:06 +0200

    Added unit test for the check data of the Text DataHandler

---
+++ tests/datahandlers/test_text.py
@@ -0,0 +1,15 @@
+import unittest
+
+from lodel.leapi.datahandlers.datas import Text
+
+
+class TextTestCase(unittest.TestCase):
+
+    def test_check_data(self):
+        test_value = """ Ceci est un texte multiligne pour tester le check
+        sur le datahandler
+        Text
+        """
+        test_text = Text()
+        _, error = test_text._check_data_value(test_value)
+        self.assertIsNone(error)
