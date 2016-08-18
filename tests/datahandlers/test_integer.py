5e171fa067ee7d4df48d4c3c097e9c913e78f81d
Roland Haroutiounian <roland.har@gmail.com>
2016-08-18 12:12:45 +0200

    Deleted two debug outputs

--- tests/datahandlers/test_integer.py
+++ tests/datahandlers/test_integer.py
@@ -12,13 +12,11 @@ class IntegerTestCase(unittest.TestCase):
         for test_bad_value in ['ok','ceci est un test', '15.2', 15.2]:
             _, error = test_int._check_data_value(test_bad_value)
             self.assertIsNotNone(error)
-            print(test_bad_value)
 
         # Correct values
         for test_correct_value in [10, '15', '15.0']:
             _, error = test_int._check_data_value(test_correct_value)
             self.assertIsNone(error)
-            print(test_correct_value)
 
     def test_can_override(self):
         test_int = Integer()
