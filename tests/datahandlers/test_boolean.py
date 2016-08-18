83a7b538081dd7c83d3dac638aa57a3fa32f7c81
Roland Haroutiounian <roland.har@gmail.com>
2016-08-18 11:07:20 +0200

    Added a test for the can_override method of the Boolean Datahandler

--- tests/datahandlers/test_boolean.py
+++ tests/datahandlers/test_boolean.py
@@ -1,6 +1,6 @@
 import unittest
 
-from lodel.leapi.datahandlers.datas import Boolean
+from lodel.leapi.datahandlers.datas import Boolean, Varchar, Integer
 
 
 class BooleanTestCase(unittest.TestCase):
@@ -17,3 +17,12 @@ class BooleanTestCase(unittest.TestCase):
         for test_value in ['ok', 'True', 'False']:
             value, error = test_boolean._check_data_value(test_value)
             self.assertIsNotNone(error)
+
+    def test_can_override(self):
+        test_boolean = Boolean()
+
+        test_varchar = Varchar()
+        test_int = Integer()
+
+        self.assertFalse(test_boolean.can_override(test_varchar))
+        self.assertFalse(test_boolean.can_override(test_int))
