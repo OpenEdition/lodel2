8316ab82ff457020708ec3d95bc0886eb730aa09
Yann <yann.weber@openedition.org>
2016-08-30 13:59:06 +0200

    Configure autotools for distributing lodel2
    IMPORTANT : tests are broken. See details below
    
    A new lodel/buildconf.py.ini file created. It will be transformed to a lodel/buildconf.py file by ./configure .
    This file goal is to store build time constant (for the moment the presence of pymongo).
    You have to build lodel2 before being able to run the tests.

--- tests/migration_handler/test_db_init.py
+++ tests/migration_handler/test_db_init.py
@@ -1,26 +1,32 @@
 # -*- coding: utf-8 -*-
-import unittest
-from plugins.mongodb_datasource.migration_handler import *
 
-class MongoDbMigrationHandlerTestCase(unittest.TestCase):
+from lodel import buildconf
 
-    def test_check_connection_args(self):
-        empty_connection_args = {}
-        with self.assertRaises(TypeError):
-            MigrationHandler(empty_connection_args)
+if not buildconf.PYMONGO:
+    import warnings
+    warnings.warn("Skipping tests about mongodb datasource. Pymongo not installed")
+else:
+    import unittest
+    from plugins.mongodb_datasource.migration_handler import *
+    class MongoDbMigrationHandlerTestCase(unittest.TestCase):
 
-        bad_connection_args_dicts = [
-            {'host': 'localhost', 'port': 20030},
-            {'host': 'localhost', 'port': 28015},
-            {'host': 'localhost', 'port': 28015, 'login':'lodel', 'password': 'lap'}
-        ]
-        for bad_connection_args_dict in bad_connection_args_dicts:
+        def test_check_connection_args(self):
+            empty_connection_args = {}
             with self.assertRaises(TypeError):
-                MigrationHandler(bad_connection_args_dict)
+                MigrationHandler(empty_connection_args)
 
-    ## @todo pass the connection arguments in the settings
-    @unittest.skip
-    def test_init_db(self):
-        correct_connection_args = {'host': 'localhost', 'port': 28015, 'username': 'lodel_admin', 'password': 'lapwd', 'db_name': 'lodel'}
-        migration_handler = MigrationHandler(correct_connection_args)
-        migration_handler.init_db()
+            bad_connection_args_dicts = [
+                {'host': 'localhost', 'port': 20030},
+                {'host': 'localhost', 'port': 28015},
+                {'host': 'localhost', 'port': 28015, 'login':'lodel', 'password': 'lap'}
+            ]
+            for bad_connection_args_dict in bad_connection_args_dicts:
+                with self.assertRaises(TypeError):
+                    MigrationHandler(bad_connection_args_dict)
+
+        ## @todo pass the connection arguments in the settings
+        @unittest.skip
+        def test_init_db(self):
+            correct_connection_args = {'host': 'localhost', 'port': 28015, 'username': 'lodel_admin', 'password': 'lapwd', 'db_name': 'lodel'}
+            migration_handler = MigrationHandler(correct_connection_args)
+            migration_handler.init_db()
