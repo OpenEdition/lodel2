899cc4e1336dc42325537bf12421a02465096d61
prieto <helene.prieto@openedition.org>
2016-09-01 14:06:24 +0200

    Sig in interface

--- plugins/webui/interface/controllers/users.py
+++ plugins/webui/interface/controllers/users.py
@@ -2,6 +2,7 @@
 from .base import get_response
 from ...exceptions import *
 from ...client import WebUiClient as WebUiClient
+from lodel.auth.client import LodelSession as Session
 from lodel import logger
 import leapi_dyncode as dyncode
 
@@ -9,7 +10,13 @@ def signin(request):
     msg=''
     if request.method == 'POST':
         WebUiClient.authenticate(request.form['inputLogin'], request.form['inputPassword'])
-        return get_response('users/welcome.html')
+        logger.debug(WebUiClient.session().datas)
+        uid=WebUiClient.session().datas['__auth_user_infos']['uid']
+        leoclass=WebUiClient.session().datas['__auth_user_infos']['leoclass']
+        query_filter=list()
+        query_filter.append((leoclass.uid_fieldname()[0],' = ', uid))
+        username = leoclass.get(query_filter)
+        return get_response('users/welcome.html', username = username)
     else:
         return get_response('users/signin.html')
 
