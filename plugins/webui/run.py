4fabe723ddc1db19dc7dae6a40d55e9f84833a26
Roland Haroutiounian <roland.har@gmail.com>
2016-09-07 10:44:39 +0200

    Cleaned useless code in webui's run module

--- plugins/webui/run.py
+++ plugins/webui/run.py
@@ -5,9 +5,7 @@ import os
 import hashlib
 import time
 
-from werkzeug.contrib.sessions import FilesystemSessionStore
 from werkzeug.wrappers import Response
-from werkzeug.contrib.securecookie import SecureCookie
 
 from lodel.settings import Settings
 from .interface.router import get_controller
@@ -15,20 +13,17 @@ from .interface.lodelrequest import LodelRequest
 from .exceptions import *
 from .client import WebUiClient
 from lodel.auth.exceptions import *
-from lodel.utils.datetime import get_utc_timestamp
-from lodel.plugin.hooks import LodelHook
 
 SESSION_FILES_BASE_DIR = Settings.webui.sessions.directory
 SESSION_FILES_TEMPLATE = Settings.webui.sessions.file_template
 SESSION_EXPIRATION_LIMIT = Settings.webui.sessions.expiration
 
-session_store = FilesystemSessionStore(path=SESSION_FILES_BASE_DIR, filename_template=SESSION_FILES_TEMPLATE)
-
+# TODO Add these informations to the configuration options (lodel2.webui.cookies)
 COOKIE_SESSION_ID = 'toktoken'
 COOKIE_SESSION_HASH = 'nekotkot'
-#COOKIE_SESSION_HASH_SALT = [ os.urandom(32) for _ in range(2) ] #Before and after salt (maybe useless)
 COOKIE_SESSION_HASH_SALT = ['salt1', 'salt2']
 COOKIE_SESSION_HASH_ALGO = hashlib.sha512
+
 
 ##@brief Return a salted hash of a cookie
 def cookie_hash(token):
@@ -58,6 +53,7 @@ def load_cookie(request):
             WebUiClient, 'Bad cookies : hash mismatch')
     return token
 
+
 ##@brief Properly set cookies and hash given a token
 #@param response
 #@param token str : the session token
@@ -65,10 +61,10 @@ def save_cookie(response, token):
     response.set_cookie(COOKIE_SESSION_ID, token)
     response.set_cookie(COOKIE_SESSION_HASH, cookie_hash(token))
 
+
 def empty_cookie(response):
     response.set_cookie(COOKIE_SESSION_ID, '')
     response.set_cookie(COOKIE_SESSION_HASH, '')
-    
 
 
 #Starting instance
@@ -77,26 +73,6 @@ loader.start()
 import lodel
 import leapi_dyncode as dyncode
 lodel.dyncode = dyncode
-
-# TODO déplacer dans un module "sessions.py"
-def delete_old_session_files(timestamp_now):
-    session_files_path = os.path.abspath(session_store.path)
-    session_files = [os.path.join(session_files_path, file_object) for file_object in os.listdir(session_files_path)
-                     if os.path.isfile(os.path.join(session_files_path, file_object))]
-
-    for session_file in session_files:
-        last_modified = os.stat(session_file).st_mtime
-        expiration_timestamp = last_modified + SESSION_EXPIRATION_LIMIT
-        if timestamp_now > expiration_timestamp:
-            os.unlink(session_file)
-
-
-def is_session_file_expired(timestamp_now, sid):
-    session_file = session_store.get_session_filename(sid)
-    expiration_timestamp = os.stat(session_file).st_mtime + SESSION_EXPIRATION_LIMIT
-    if timestamp_now < expiration_timestamp:
-        return False
-    return True
 
 
 # WSGI Application
@@ -130,7 +106,7 @@ def application(env, start_response):
                 return res
         session_token = WebUiClient.session_token()
         if session_token is not None:
-            save_cookie(response,session_token)
+            save_cookie(response, session_token)
         session_token = None
     except (ClientError, ClientAuthenticationError):
         response = HttpException(200).render(request)
