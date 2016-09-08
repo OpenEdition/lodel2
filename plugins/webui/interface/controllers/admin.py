500b75d86c4aa74e94a3b08593573fe17a7d6eed
prieto <helene.prieto@openedition.org>
2016-09-08 12:33:19 +0200

    Changes in the interface - adding delete option

--- plugins/webui/interface/controllers/admin.py
+++ plugins/webui/interface/controllers/admin.py
@@ -36,9 +36,13 @@ def admin_update(request):
 
         for in_put, in_value in request.form.items():
             if in_put != 'classname' and  in_put != 'uid':
-                fields[in_put[12:]] = in_value
-            #elif in_put == 'classname':
-            #    fields['classname'] = in_value
+                dhl = target_leo.data_handler(in_put[12:])
+                if dhl.is_reference() and in_value != '':
+                    in_value.replace(" ","")
+                    in_value=in_value.split(',')
+                    in_value=list(in_value)
+                if in_value != '':
+                    fields[in_put[12:]] = in_value
 
         filter_q = '%s = %s' % (uid_field, uid)
         obj = (target_leo.get((filter_q)))[0]
@@ -130,6 +134,50 @@ def admin_create(request):
         raise HttpException(400)
     return get_response('admin/admin_create.html', target=target_leo)
 
+def admin_delete(request):
+    if WebUiClient.is_anonymous():
+        return get_response('users/signin.html')
+    classname = None
+
+    if 'classname' in request.GET:
+        classname = request.GET['classname']
+        if len(classname) > 1:
+            raise HttpException(400)
+        classname = classname[0]
+        try:
+            target_leo = dyncode.Object.name2class(classname)
+        except LeApiError:
+            # classname = None
+            raise HttpException(400)
+        logger.warning('Composed uids broken here')
+        uid_field = target_leo.uid_fieldname()[0]
+
+    test_valid = 'lodel_id' in request.GET \
+        and len(request.GET['lodel_id']) == 1
+
+    if test_valid:
+        try:
+            dh = target_leo.field(uid_field)
+            lodel_id = dh.cast_type(request.GET['lodel_id'][0])
+        except (ValueError, TypeError):
+            test_valid = False
+
+    if not test_valid:
+        raise HttpException(400)
+    else:
+        query_filters = list()
+        query_filters.append((uid_field,'=',lodel_id))
+        nb_deleted = target_leo.delete_bundle(query_filters)
+
+    if nb_deleted == 1:
+            msg = 'Object successfully deleted';
+    else:
+            msg = 'Oops something wrong happened...object still here'
+            
+    return get_response('admin/admin_delete.html', target=target_leo, lodel_id =lodel_id, msg = msg)
+
+        
+        
 def admin_classes(request):
     if WebUiClient.is_anonymous():
         return get_response('users/signin.html')
@@ -139,6 +187,11 @@ def create_object(request):
     if WebUiClient.is_anonymous():
         return get_response('users/signin.html')
     return get_response('admin/list_classes_create.html', my_classes = dyncode.dynclasses)
+
+def delete_object(request):
+    if WebUiClient.is_anonymous():
+        return get_response('users/signin.html')
+    return get_response('admin/list_classes_delete.html', my_classes = dyncode.dynclasses)
     
 def admin_class(request):
     if WebUiClient.is_anonymous():
@@ -155,7 +208,23 @@ def admin_class(request):
     if classname is None or target_leo.is_abstract():
         raise HttpException(400)
     return get_response('admin/show_class_admin.html', target=target_leo)
-   
+
+def delete_in_class(request):
+    if WebUiClient.is_anonymous():
+        return get_response('users/signin.html')
+    if 'classname' in request.GET:
+        classname = request.GET['classname']
+        if len(classname) > 1:
+            raise HttpException(400)
+        classname = classname[0]
+        try:
+            target_leo = dyncode.Object.name2class(classname)
+        except LeApiError:
+            classname = None
+    if classname is None or target_leo.is_abstract():
+        raise HttpException(400)
+    return get_response('admin/show_class_delete.html', target=target_leo)
+
 def admin(request):
     if WebUiClient.is_anonymous():
         return get_response('users/signin.html')
