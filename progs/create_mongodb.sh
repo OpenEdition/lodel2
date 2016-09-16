9ad078422017cb763d8204d8ed56cb0cfed5d96f
prieto <helene.prieto@openedition.org>
2016-09-16 16:14:41 +0200

    Take car of port in creation of databases

--- progs/create_mongodb.sh
+++ progs/create_mongodb.sh
@@ -1,41 +1,42 @@
 #! /bin/bash
 
 usage() {
-	echo -e "Usage : $0 host database newuser_identifier newuser_pwd (admin_identifier|config_file) [admin_pwd]" 1>&2
+	echo -e "Usage : $0 host port database newuser_identifier newuser_pwd (admin_identifier|config_file) [admin_pwd]" 1>&2
     echo -e "config_file has to define ADMIN and ADMINPWD" 1>&2
 	exit 1
 }
 
-if [ $# -lt 5 ]
+if [ $# -lt 6 ]
 then
 	echo "Not enough arguments" 1>&2
 	usage
 fi
 
-if [ $# -eq 5 ]
+if [ $# -eq 6 ]
 then
-    if [ ! -f $5 ]
+    if [ ! -f $6 ]
     then  
-	    echo "Not enough arguments or the configation file $5 doesn't exist" 1>&2
+	    echo "Not enough arguments or the configation file $6 doesn't exist" 1>&2
         usage
     else
-	    . $5
+	    . $6
     fi
 fi
 
-if [ $# -eq 6 ]
+if [ $# -eq 7 ]
 then
-    ADMIN=$5
-    ADMINPWD=$6
+    ADMIN=$6
+    ADMINPWD=$7
 fi
 
 host=$1
-db=$2
-newuser=$3
-newuserpwd=$4
+port=$2
+db=$3
+newuser=$4
+newuserpwd=$5
 
-mongo $1/admin -u $ADMIN -p $ADMINPWD <<EOF
-db.addUser('$3', '$4')
+mongo $1:$2/admin -u $ADMIN -p $ADMINPWD <<EOF
+db.addUser('$4', '$5')
 use $db
 quit()
 EOF
