44a674d99ba81ecfe9827db0e68dbd9e200c344f
Roland Haroutiounian <roland.har@gmail.com>
2016-11-04 14:09:37 +0100

    The preload function is getting its contexts directly from LodelContexts._contexts

--- lodel/plugins/multisite/loader.py
+++ lodel/plugins/multisite/loader.py
@@ -4,16 +4,8 @@ from lodel.context import LodelContext
 
 
 def preload():
-    # TODO Get this path dynamically
-    LODEL_BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-    LODEL_INSTANCES_DIR = os.path.join(LODEL_BASE_DIR, 'lodelsites')
-    LodelContext.init(LodelContext.MULTISITE)
-    lodelsites_list = [sitename for sitename in os.listdir(LODEL_INSTANCES_DIR) if os.path.isdir(sitename)]
-
-    for lodelsite_path in lodelsites_list:
-        LodelContext.from_path(lodelsite_path)
-        lodelcontext = LodelContext._contexts[lodelsite_path.split('/')[-1]]
+    for lodelcontext in LodelContext._contexts:
 
         # Loading settings
         lodelcontext.expose_modules(globals(), {'lodel.settings.settings': [('Settings', 'settings')]})
