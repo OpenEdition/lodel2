9a8a615893d41122a04c1dcb1428d64bbbf86bc8
Roland Haroutiounian <roland.har@gmail.com>
2016-11-04 13:58:23 +0100

    Added a loader module in the multisite plugin

---
+++ lodel/plugins/multisite/loader.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import os
+from lodel.context import LodelContext
+
+
+def preload():
+    # TODO Get this path dynamically
+    LODEL_BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+    LODEL_INSTANCES_DIR = os.path.join(LODEL_BASE_DIR, 'lodelsites')
+    LodelContext.init(LodelContext.MULTISITE)
+    lodelsites_list = [sitename for sitename in os.listdir(LODEL_INSTANCES_DIR) if os.path.isdir(sitename)]
+
+    for lodelsite_path in lodelsites_list:
+        LodelContext.from_path(lodelsite_path)
+        lodelcontext = LodelContext._contexts[lodelsite_path.split('/')[-1]]
+
+        # Loading settings
+        lodelcontext.expose_modules(globals(), {'lodel.settings.settings': [('Settings', 'settings')]})
+        lodelcontext.expose_modules(globals(), {'lodel.settings': ['Settings']})
+
+        # Loading hooks
+        LodelContext.expose_modules(globals(), {
+            'lodel.plugin': ['LodelHook'],
+            'lodel.plugin.core_hooks': 'core_hooks',
+            'lodel.plugin.core_scripts': 'core_scripts'
+        })
