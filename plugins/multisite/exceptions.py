568f00794bf0a1bccd34a0838c676287dbfcd626
prieto <helene.prieto@openedition.org>
2016-12-07 15:58:38 +0100

    Revert "Merge branch 'poc_multisite' into new_em"
    This reverts commit cd902df2a5146dbcafa686aea0640d61897a25d6, reversing
    changes made to e53d9f5ac44404b40a0b489722e2d1de6cd7fcf0.

--- plugins/multisite/exceptions.py
+++
@@ -1,8 +0,0 @@
-##@brief Designed to be the base class for all exceptions of this module
-class MultiSiteError(Exception):
-    pass
-
-##@brief Designed to be thrown on site identifier errors
-class MultiSiteIdentifierError(MultiSiteError):
-    pass
-
