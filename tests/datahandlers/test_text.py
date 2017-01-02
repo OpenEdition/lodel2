ff5c13929aa85aaa8da1918bbf1b61b9b1e576f8
Quentin Bonaventure <quentin.bonaventure@openedition.org>
2017-01-02 10:26:25 +0100

    Proposed change for test_text.py

--- tests/datahandlers/test_text.py
+++ tests/datahandlers/test_text.py
@@ -1,15 +1,27 @@
 import unittest
 
 from lodel.leapi.datahandlers.datas import Text
+from lodel.exceptions import FieldValidationError
 
 
 class TextTestCase(unittest.TestCase):
+    
+    
+    def test_base_type_property_value_equals_text(self):
+        self.assertEqual(Text.base_type, 'text')
 
-    def test_check_data(self):
+
+    def test_non_string_value_throws_FieldValidationError(self):
+        self.assertRaises(
+            FieldValidationError,
+            Text()._check_data_value,
+            bool
+            )
+    
+
+    def test_check_data_returns_unchanged_strnig_parameter_if_valid(self):
         test_value = """ Ceci est un texte multiligne pour tester le check
         sur le datahandler
         Text
         """
-        test_text = Text()
-        _, error = test_text.check_data_value(test_value)
-        self.assertIsNone(error)
+        self.assertEqual(Text()._check_data_value(test_value), test_value)
