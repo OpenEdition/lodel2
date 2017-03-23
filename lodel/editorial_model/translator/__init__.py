d23e0fd9d4d34b70ea0ed039898f1a4cdb1d2656
Roland Haroutiounian <roland.har@gmail.com>
2017-03-23 09:41:10 +0100

    Added documentation to the lodel.editorial_model.translator package

--- lodel/editorial_model/translator/__init__.py
+++ lodel/editorial_model/translator/__init__.py
@@ -1,4 +1,4 @@
 ## @package lodel.editorial_model.translator Editorial model translators
 #
-# This package contains modules that provides a save and a load function able to load and save
-# lodel.editorial_model.model.EditorialModel
+# This package is dedicated to the translation of an EditorialModel as several formats like pickle files or XML files.
+# Each module provides save and load functions to read/write an lodel.editorial_model.model.EditorialModel object from and to a file.
