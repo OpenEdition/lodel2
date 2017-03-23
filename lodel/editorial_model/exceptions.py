d39f9784c453b53cd8e9c41b6e4391c663a98fce
Roland Haroutiounian <roland.har@gmail.com>
2017-03-23 09:29:52 +0100

    Changed the documentation of the lodel.editorial_model.exceptions module

--- lodel/editorial_model/exceptions.py
+++ lodel/editorial_model/exceptions.py
@@ -1,5 +1,9 @@
 #-*- coding: utf-8 -*-
 
+## @package lodel.editorial_model.exceptions
+# This module contains the specific exceptions related to the EditorialModel Management.
+
+
 class EditorialModelError(Exception):
     pass
 
@@ -7,7 +11,7 @@ class EditorialModelError(Exception):
 def assert_edit():
     try:
         from lodel import Settings
-    except ImportError: #Very dirty, but don't know how to fix the tests
+    except ImportError:  # Very dirty, but don't know how to fix the tests
         return
     if not Settings.editorialmodel.editormode:
         raise EditorialModelError("EM is readonly : editormode is OFF")
