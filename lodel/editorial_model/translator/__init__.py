38997530236797b3e4fc1ae11b930bd5b4de95e8
Roland Haroutiounian <roland.har@gmail.com>
2017-03-23 11:43:06 +0100

    Documentation on the lodel.leapi.datahandlers.datas module

--- lodel/editorial_model/translator/__init__.py
+++ lodel/editorial_model/translator/__init__.py
@@ -1,4 +1,4 @@
 ## @package lodel.editorial_model.translator Editorial model translators
 #
-# This package is dedicated to the translation of an EditorialModel as several formats like pickle files or XML files.
+# This package is dedicated to the translation of an EditorialModel as several formats like pickle files or XML files. \n
 # Each module provides save and load functions to read/write an lodel.editorial_model.model.EditorialModel object from and to a file.
