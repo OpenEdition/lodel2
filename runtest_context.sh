02cc9998162b6ce77fdd797dce73b14516fd9990
Quentin Bonaventure <quentin@bonaventure.xyz>
2017-05-04 08:52:13 +0200

    Licensing follow up

--- runtest_context.sh
+++ runtest_context.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-#
+# 
 # This file is part of Lodel 2 (https://github.com/OpenEdition)
 #
 # Copyright (C) 2015-2017 Cléo UMS-3287
 #
-# This program is free software: you can redistribute it and/or  modify
-# it under the terms of the GNU Affero General Public License, version 3,
-# as published by the Free Software Foundation.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
--- tests/datahandlers/test_concat.py
+++ tests/datahandlers/test_concat.py
@@ -1,3 +1,23 @@
+# 
+# This file is part of Lodel 2 (https://github.com/OpenEdition)
+#
+# Copyright (C) 2015-2017 Cléo UMS-3287
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
 import unittest
 
 from lodel.leapi.datahandlers.datas import Concat as Testee
@@ -20,3 +40,4 @@ class ConcatTestCase(unittest.TestCase):
         testee = Testee(field_list, separator)
         
         self.assertEqual(testee._format_string, separator.join(['%s' for _ in field_list]))
+        
