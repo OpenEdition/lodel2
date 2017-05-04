ec1e531aa2b4c79211fbb004f092bba70bbaf0f0
Quentin Bonaventure <quentin@bonaventure.xyz>
2017-05-04 08:42:40 +0200

    Most of licence headers + LICENCE.md

--- tests/leapi/test_leobject.py
+++ tests/leapi/test_leobject.py
@@ -1,3 +1,22 @@
+#
+# This file is part of Lodel 2 (https://github.com/OpenEdition)
+#
+# Copyright (C) 2015-2017 Cléo UMS-3287
+#
+# This program is free software: you can redistribute it and/or  modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
 import unittest
 from unittest import mock
 from unittest.mock import patch
