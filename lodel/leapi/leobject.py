950abfa726058775f81a9c4dbf3ce4ae02e5eb65
prieto <helene.prieto@openedition.org>
2017-05-04 09:32:56 +0200

    mainpage for doxygen

--- lodel/leapi/leobject.py
+++ lodel/leapi/leobject.py
@@ -17,7 +17,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-
 ## @package lodel.leapi.leobject
 # This module is centered around the basic LeObject class, which is the main class for all the objects managed by lodel.
 
